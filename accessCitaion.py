78286866a8deb72717c2949f79cf818cd00a271e
RIST-tinoue <49736919+RIST-tinoue@users.noreply.github.com>
2019-12-13 16:26:30 +0900

    Start to implement accessCitaion.py.

---
+++ accessCitaion.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8-*-
+"""\
+Get CMIP6 Citation info with the experiment granularity.
+
+---
+This script gets experiment-granularity info from the citation service.
+
+You have to specify MIP(`activity_id`), model(`source_id`),
+experiment(`experiment_id`) to get info.
+
+"""
+import json
+import certifi
+import urllib3
+import argparse
+
+__author__ = 'T.Inoue'
+__credits__ = 'Copyright (c) 2019 JAMSTEC'
+__version__ = 'v20191213'
+__date__ = '2019/12/13'
+
+baseGetUrl = 'https://cera-www.dkrz.de/WDCC/ui/cerasearch/cerarest/exportcmip6'
+basePostUrl = "http://ceracite.dkrz.de:5000/api/v1/citation"
+
+
+mip = 'CMIP'
+model = 'MIROC6'
+institution = 'MIROC'
+experiment = 'historical'
+
+# mip = 'HighResMIP'
+# model = 'NICAM16-7S'
+# institution = 'MIROC'
+# experiment = 'highresSST-present'
+
+desc, epilog = __doc__.split('---')
+
+
+def my_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=desc, epilog=epilog, )
+    parser.add_argument('-v', '--verbose',
+                        dest='verbose', action='store_true',
+                        help='be verbose.',
+                        default=False)
+    parser.add_argument('-a', '--mip', '--activity_id',
+                        type=str,
+                        help='MIP(activity_id)',
+                        default=mip)
+    parser.add_argument('-i', '--inst', '--institution_id',
+                        metavar='inst', type=str,
+                        help='inst(institution_id)',
+                        default=institution)
+    parser.add_argument('-s', '--model', '--source_id',
+                        type=str,
+                        help='model(source_id)',
+                        default=model)
+    parser.add_argument('-e', '--exp', '--experiment_id',
+                        metavar='exp', type=str,
+                        help='experiments to submit',
+                        default=experiment)
+
+    return parser
+
+
+def getJSON(source_id=None, activity_id=None,
+            institution_id=None, experiment_id=None):
+    """
+    Access Citation web cite and get JSON for given CV's.
+    """
+
+    drs = '.'.join(['CMIP6', activity_id, institution_id, source_id])
+    if experiment_id is not None:
+        drs += '.' + experiment_id
+    fields = {'input': drs}
+
+    print('HTTP access with DRS:', drs)
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
+    r = http.request_encode_url('GET', baseGetUrl, fields=fields)
+
+    if (r.status != 200):
+        print('Bad Status:', r.status)
+        d = eval(r.data.decode('utf-8'))
+        print(d['error'])
+        return None
+    jsonData = json.loads(r.data.decode('utf-8'))
+    return jsonData
+
+
+def main():
+
+    parser = my_parser()
+    a = parser.parse_args()
+
+    if (a.verbose):
+        print('Configuration:')
+        print('  mip:', a.mip)
+        print('  model:', a.model)
+        print('  institution:', a.inst)
+        print('  experiments:', a.exp)
+
+
+    base = getJSON(source_id=a.model, activity_id=a.mip, institution_id=a.inst, experiment_id=a.exp)
+    if (base is None):
+        exit(1)
+
+    base_title = base['titles'][0]
+    base_subject = base['subjects'][0]['subject']
+
+    if (a.verbose):
+        print('base title:', base_title)
+        print('base subject:', base_subject)
+
+    fname = base_subject + '.json'
+    with open(fname, 'w') as f:
+        print('Saving base data to:', fname)
+        json.dump(base, f, indent=2)
+    print('Done.')
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
