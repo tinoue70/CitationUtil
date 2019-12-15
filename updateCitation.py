b7e6282343a4e02407ec625c2f0f696a639b39c7
RIST-tinoue <49736919+RIST-tinoue@users.noreply.github.com>
2019-12-15 20:43:32 +0900

    Moved JSON handling routines to utils.py.

--- updateCitation.py
+++ updateCitation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8-*-
+
 """\
 Submit CMIP6 Citation info of experiment granularity.
 
@@ -12,19 +13,15 @@ You have to specify MIP(`activity_id`), model(`source_id`) to get base
 info, and have to specify experiments(`experiment_id`) to submit
 experiment-granularity info.
 """
-from utils import getJSON
+
 import json
-import certifi
-import urllib3
 import argparse
+from utils import loadJSON, getJSON, postJSON, setJSONfname
 
 __author__ = 'T.Inoue'
 __credits__ = 'Copyright (c) 2019 RIST'
 __version__ = 'v20190626'
 __date__ = '2019/06/26'
-
-# baseGetUrl = 'https://cera-www.dkrz.de/WDCC/ui/cerasearch/cerarest/exportcmip6'
-# basePostUrl = "http://ceracite.dkrz.de:5000/api/v1/citation"
 
 mip = 'CMIP'
 model = 'MIROC6'
@@ -48,9 +45,9 @@ def my_parser():
                         dest='verbose', action='store_true',
                         help='be verbose.',
                         )
-    parser.add_argument('-c', '--checkonly',
-                        dest='checkonly', action='store_true',
-                        help='Perform check only.')
+    parser.add_argument('-p', '--post',
+                        dest='dopost', action='store_true',
+                        help='Do POST.')
     parser.add_argument('-g', '--getonly',
                         dest='getonly', action='store_true',
                         help='Get base JSON, only.')
@@ -77,54 +74,6 @@ def my_parser():
     return parser
 
 
-# def getJSON(source_id='MIROC6', activity_id='CMIP',
-#             institution_id='MIROC', experiment_id=None):
-#     """
-#     Access Citation web cite and get JSON for given CV's.
-#     """
-
-#     drs = '.'.join(['CMIP6', activity_id, institution_id, source_id])
-#     if experiment_id is not None:
-#         drs += '.' + experiment_id
-#     fields = {'input': drs}
-
-#     print('HTTP access with DRS:', drs)
-#     http = urllib3.PoolManager(
-#         cert_reqs='CERT_REQUIRED',
-#         ca_certs=certifi.where())
-#     r = http.request_encode_url('GET', baseGetUrl, fields=fields)
-
-#     if (r.status != 200):
-#         print('Bad Status:', r.status)
-#         d = eval(r.data.decode('utf-8'))
-#         print(d['error'])
-#         return None
-#     jsonData = json.loads(r.data.decode('utf-8'))
-#     return jsonData
-
-
-def loadJSON(fname=None,
-             source_id=None, activity_id=None,
-             institution_id='MIROC', experiment_id=None):
-    """
-    Load local JSON file instead of accessing Citation web cite.
-    """
-
-    if fname is None or fname is '':
-        if not all([source_id, activity_id, institution_id]):
-            return None
-        else:
-            fname = '.'.join(['CMIP6', activity_id, institution_id, source_id])
-        if (experiment_id is not None):
-            fname += '.' + experiment_id
-        fname += '.json'
-    print(f'Loading from "{fname}"')
-
-    with open(fname) as f:
-        jsonData = json.load(f)
-    return jsonData
-
-
 def addExperiment(inData, experiment):
     """
     Convert from MIP granularity data to experiment granularity data;
@@ -148,56 +97,6 @@ def addExperiment(inData, experiment):
     return data
 
 
-def putBack(data, extra='check'):
-    """
-    Put back modified JSON to the citaion web.
-
-    Based on "citation_client.py".
-
-    `extra` must be ``test``, ``check``, ``tcheck`` or ``None``.
-
-    Returns response status of request.
-
-    Note that if `extra` is ``check``, response status is other than 200
-    if something is wrong.
-
-    """
-
-    import netrc
-
-    url = basePostUrl
-    extra_list = ('test', 'check', 'tcheck')
-    if (extra is not None) and (extra in extra_list):
-        extra_param = '?' + extra + '=1'
-        url += extra_param
-
-    info = netrc.netrc()
-    login, account, password = info.authenticators("cera")
-    headers = urllib3.util.make_headers(basic_auth=login+':'+password)
-    headers['Content-Type'] = "application/json"
-
-    http = urllib3.PoolManager(
-        cert_reqs='CERT_REQUIRED',
-        ca_certs=certifi.where())
-    encoded_body = json.dumps(data).encode('utf-8')
-
-    try:
-        r = http.request_encode_body('POST', url,
-                                     headers=headers, body=encoded_body)
-    except Exception as e:
-        print('Error:', e)
-        return e
-
-    if (r.status != 200):
-        print('Bad Status:', r.status)
-        d = eval(r.data.decode('utf-8'))
-        print(d['error'])
-    else:
-        print(r.data.decode('utf-8'))
-
-    return r.status
-
-
 def main():
 
     parser = my_parser()
@@ -205,7 +104,7 @@ def main():
 
     if (a.verbose):
         print('Configuration:')
-        print('  checkonly:', a.checkonly)
+        print('  dopost:', a.dopost)
         print('  getonly:', a.getonly)
         print('  model:', a.model)
         print('  mip:', a.mip)
@@ -215,12 +114,17 @@ def main():
         elif (a.loadfile):
             print('  loadfile: specified explicitly:', a.loadfile)
         else:
-            print('  loadfile: specified but no a.')
+            a.loadfile = setJSONfname(source_id=a.model, activity_id=a.mip,
+                                      institution_id=a.inst)
+            print('  loadfile: set as:', a.loadfile)
 
     if (a.loadfile is None):
-        base = getJSON(source_id=a.model, activity_id=a.mip, institution_id=a.inst)
+        base = getJSON(source_id=a.model, activity_id=a.mip,
+                       institution_id=a.inst)
     else:
-        base = loadJSON(a.loadfile, source_id=a.model, activity_id=a.mip)
+        if (a.verbose):
+            print('file:', a.loadfile)
+        base = loadJSON(a.loadfile)
 
     if (base is None):
         exit(1)
@@ -242,13 +146,13 @@ def main():
             data = addExperiment(base, exp)     # base is preserved.
             data_subject = data['subjects'][0]['subject']
             print('Checking', data_subject)
-            status = putBack(data, extra='check')
+            status = postJSON(data, extra='check')
             if (not isinstance(status, Exception)):
                 if (a.verbose):
                     print("Check status:", status)
-                if (status == 200) and (not a.checkonly):
+                if (status == 200) and (a.dopost):
                     print('Submitting', data_subject)
-                    status = putBack(data, extra=None)
+                    status = postJSON(data, extra=None)
                     if (status != 200):
                         print(status)
     print('Done.')
--- utils.py
+++ utils.py
@@ -17,16 +17,18 @@ __date__ = '2019/12/13'
 baseGetUrl = 'https://cera-www.dkrz.de/WDCC/ui/cerasearch/cerarest/exportcmip6'
 basePostUrl = "http://ceracite.dkrz.de:5000/api/v1/citation"
 
+
 class Person():
-    def __init__(self, givenName=None, familyName=None, email=None, affiliation=None):
+    def __init__(self, givenName=None, familyName=None,
+                 email=None, affiliation=None):
         self.givenName = givenName
         self.familyName = familyName
         self.email = email
         self.affiliation = affiliation
 
-
     def __str__(self):
         return f'{self.creatorName}, {self.email}, {self.affiliation}'
+
 
 class Creator(Person):
     def __init__(self, *args):
@@ -63,8 +65,81 @@ def getJSON(source_id=None, activity_id=None,
     return jsonData
 
 
+def loadJSON(fname=None):
+    """
+    Load local JSON file instead of accessing Citation web cite.
+    """
+
+    with open(fname) as f:
+        jsonData = json.load(f)
+    return jsonData
+
+
+def setJSONfname(source_id=None, activity_id=None,
+                 institution_id=None, experiment_id=None):
+    """
+    Construct JSON filename.
+    """
+
+    if not all([source_id, activity_id, institution_id]):
+        return None
+    else:
+        fname = '.'.join(['CMIP6', activity_id, institution_id, source_id])
+    if (experiment_id is not None):
+        fname += '.' + experiment_id
+    fname += '.json'
+
+    return fname
+
+
+def postJSON(jsonData, extra='check'):
+    """
+    Post JSON to the citaion web.
+
+    Based on "citation_client.py".
+
+    `extra` must be ``test``, ``check``, ``tcheck`` or ``None``.
+
+    Returns response status of request.
+
+    Note that if `extra` is ``check``, response status is other than 200
+    if something is wrong.
+    """
+    import netrc
+
+    url = basePostUrl
+    extra_list = ('test', 'check', 'tcheck')
+    if (extra is not None) and (extra in extra_list):
+        extra_param = '?' + extra + '=1'
+        url += extra_param
+
+    info = netrc.netrc()
+    login, account, password = info.authenticators("cera")
+    headers = urllib3.util.make_headers(basic_auth=login+':'+password)
+    headers['Content-Type'] = "application/json"
+
+    http = urllib3.PoolManager(
+        cert_reqs='CERT_REQUIRED',
+        ca_certs=certifi.where())
+    encoded_body = json.dumps(jsonData).encode('utf-8')
+
+    try:
+        r = http.request_encode_body('POST', url,
+                                     headers=headers, body=encoded_body)
+    except Exception as e:
+        print('Error:', e)
+        return e
+
+    if (r.status != 200):
+        print('Bad Status:', r.status)
+        d = eval(r.data.decode('utf-8'))
+        print(d['error'])
+    else:
+        print(r.data.decode('utf-8'))
+
+    return r.status
+
 
 if __name__ == '__main__':
-    c = Creator( 'Takahiro', 'Inoue', 'tkhr_i@jamstec.go.jp', 'RESTEC')
+    c = Creator('Takahiro', 'Inoue', 'tkhr_i@jamstec.go.jp', 'RESTEC')
     print(c)
-    
